4bcd890273463339d03e37fbf0b8677bc3db72bd
G Thimmaraju <thimmaraju418@gmail.com>
2024-12-19 11:44:14 +0530

    wwe3

--- cypress/e2e/dragandDrop.cy.js
+++ cypress/e2e/dragandDrop.cy.js
@@ -15,27 +15,26 @@ describe('DD spec', () => {
 
       const dataTransfer = new DataTransfer();
 
-      cy.get('#menu-hamburger').trigger('dragstart',{
-    
-        dataTransfer
-      })
+      cy.get('#menu-hamburger').trigger('dragstart',{ dataTransfer})
 
-     cy.get('#plate-items').trigger('drop',{
+     cy.get('#plate-items').trigger('drop',{dataTransfer})
 
-        dataTransfer
-      })
+      cy.get('#menu-ice-cream').trigger('dragstart',{dataTransfer})
 
-      cy.get('#menu-ice-cream').trigger('dragstart',{
-    
-        dataTransfer
-      })
-
-     cy.get('#plate-items').trigger('drop',{
-
-        dataTransfer
-      })
+     cy.get('#plate-items').trigger('drop',{dataTransfer})
 
 
 
+    })
+
+
+    it.only("scrolling - example ", ()=>{
+
+      cy.visit('https://docs.cypress.io/app/get-started/why-cypress')
+
+      cy.get('#Our-mission').scrollIntoView()
+
+      cy.wait(3000)
+      cy.get('a[href="https://github.com/cypress-io/cypress-realworld-app"]').last().scrollIntoView()
     })
   })
--- cypress/e2e/tables2.cy.js
+++ cypress/e2e/tables2.cy.js
@@ -8,7 +8,10 @@ describe("suitename", () => {
 
         cy.get('table').should("have.length", 2) //- How many tables
 
-        cy.get('#table1 >tbody >tr').should("have.length", 4) //- How manu rows
+        cy.get('#table1 >tbody >tr').should("have.length", 4) //- How many rows
+
+
+        cy.get('#table1 >tbody >tr').its("length").should(('be.within', 5, 10))
 
         cy.get('#table1 >tbody >tr:nth-child(1) >td').should("have.length", 6) //- How many coloumns
 
@@ -23,20 +26,47 @@ describe("suitename", () => {
             expect(textvalue).to.equal("fbach@yahoo.com")
         })
 
+        cy.get('#table1>tbody>tr:nth-child(1)>td:nth-child(2)').then((text)=>{
 
-       //other way
+            var textvalue = text.text()
+           expect(textvalue).to.equal("John")
+       })
+       
+       cy.get('#table1>tbody>tr:nth-child(4)>td:nth-child(1)').then((text)=>{
+       
+            var textvalue = text.text()
+           expect(textvalue).to.equal("Conway")
+       })
+       
+       cy.get('#table1>tbody>tr:nth-child(3)>td:nth-child(4)').then((text)=>{
+       
+            var textvalue = text.text()
+           expect(textvalue).to.equal("$100.00")
+       })
 
-        cy.get('#table1 >tbody>tr:nth-child(2) >td:nth-child(3)').should('have.text', 'fbach@yahoo.com')
+    //other way
+
+    cy.get('#table1 >tbody>tr:nth-child(2) >td:nth-child(3)').should('have.text', 'fbach@yahoo.com')
 
 
+     cy.get('#table1 >tbody>tr:nth-child(1)').within(()=>{
+      
+         cy.get("td").eq(0).should('have.text', 'Smith')
+         cy.get("td").eq(1).should('have.text', 'John')
+         cy.get("td").eq(2).should('have.text', 'jsmith@gmail.com')
+         
+     })
 
-        cy.contains("Doe").parent().within(() => {
 
-            cy.get("td").eq(5).find('a[href="#delete"]').click()
+    cy.contains("Bach").parent().within(() => {
 
-            cy.get("td").eq(3).should("have.text", "$100.00")
+            //cy.get("td").eq(5).find('a[href="#delete"]').click()
 
-        })
+            cy.get("td").eq(3).should("have.text", "$51.00")
+
+     })
+
+     //Print entire table data
         cy.get('#table1 > tbody >tr').each((rows) => {
 
             cy.wrap(rows).within((celldata) => {
